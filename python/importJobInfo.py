ecf64e0687f2403d26fa531e6589f5b7aa0ce214
psittikul <patricia.sitt@gmail.com>
2019-07-22 18:05:49 -0400

    Working on Python scripts to scrape job posting/company etc. websites for data

--- python/importJobInfo.py
+++ python/importJobInfo.py
@@ -2,3 +2,5 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+url='https://careers.nintendo.com/job-openings/listing/1900000059.html?src=CWS-10000'
+response = requests.get(url)
