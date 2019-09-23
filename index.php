66c370d0907fe4b5d228e25c5865baaa3924ce67
psittikul <patricia.sitt@gmail.com>
2019-09-23 18:47:35 -0400

    Implementing Postgresql for real now?

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 require('vendor/autoload.php');
-$app = new Symfony\Application();
+$app = new Silex\Application();
 $app['debug'] = true;
 // Register the monolog logging service
-$app->register(new Symfony\Provider\MonologServiceProvider(), array(
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 // Register view rendering
-$app->register(new Symfony\Provider\TwigServiceProvider(), array(
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__ . '/views',
 ));
 // Our web handlers
@@ -19,7 +19,7 @@ $app->run();
 // Heroku PDO setup code
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(
-  new Csanquer\Symfony\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+  new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
   array(
     'pdo.server' => array(
       'driver'   => 'pgsql',
