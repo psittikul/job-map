862ea6f4b0c0d2f75f3f2880e7c27723d88467ec
psittikul <patricia.sitt@gmail.com>
2019-09-23 18:41:01 -0400

    Setting up Postgresql

--- index.php
+++ index.php
@@ -1,48 +1,48 @@
+<?php
+require('../vendor/autoload.php');
+$app = new Symfony\Application();
+$app['debug'] = true;
+// Register the monolog logging service
+$app->register(new Symfony\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+// Register view rendering
+$app->register(new Symfony\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__ . '/views',
+));
+// Our web handlers
+$app->get('/', function () use ($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+$app->run();
+// Heroku PDO setup code
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(
+  new Csanquer\Symfony\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+  array(
+    'pdo.server' => array(
+      'driver'   => 'pgsql',
+      'user' => $dbopts["user"],
+      'password' => $dbopts["pass"],
+      'host' => $dbopts["host"],
+      'port' => $dbopts["port"],
+      'dbname' => ltrim($dbopts["path"], '/')
+    )
+  )
+);
 
-<?php include "includes/header.php"; ?>
+$app->get('/db/', function () use ($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
 
-<body>
-  <?php
-  // Array mapping URL parameters to the PHP file that should be included
-  $includesArray = array(
-    "addjob" => "includes/addJob.php",
-    "addcompany" => "includes/addCompany.php",
-    "addlocation" => "includes/addLocation.php",
-    "viewcompany" => "includes/viewCompany.php"
-  );
-  // If no URL parameters are set, just show the plain index
-  if (!isset($_GET["action"]) && !isset($_GET["item"])) {
-    include "includes/main.php";
-  } else {
-    $key = htmlspecialchars($_GET["action"]) . htmlspecialchars($_GET["item"]);
-    include $includesArray[$key];
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
   }
 
-  ?>
-  <!-- Status message modal -->
-  <div class="modal" id="statusModal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title"></h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <p></p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn action-btn"></button>
-          <button type="button" class="btn action-btn"></button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Bootstrap scripts -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
