0339e806bddec09f6cdddc86efdf0f93165079ca
psittikul <patricia.sitt@gmail.com>
2019-09-23 22:38:24 -0400

    Figuring out how to get last insert ID from save company AJAX call

--- ajax/saveCompany.php
+++ ajax/saveCompany.php
@@ -15,7 +15,7 @@ if ($company_id > 0) {
     $insertQuery = "INSERT INTO company(company_name, company_website, company_glassdoor, currently_hiring, number_of_employees, remote_work)
     VALUES('$company_name', '$company_website', '$company_glassdoor', '$currently_hiring', '$number_of_employees', '$remote_work') RETURNING company_id";
     if ($result = pg_query($connection, $insertQuery)) {
-        $res = var_dump(pg_fetch_object($result));
+        $res = var_dump($result);
         echo json_encode(array("status"=>0, "message"=>"Successfully inserted new company", "id"=>$res));
     } else {
         echo json_encode(array("status" => -1, "message" => pg_last_error($connection)));
