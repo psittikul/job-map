b6417a9dccf9462d6082305f5873e7bff9910c6d
psittikul <patricia.sitt@gmail.com>
2019-09-24 04:23:40 -0400

    Fixing add job function

--- ajax/saveJob.php
+++ ajax/saveJob.php
@@ -1,8 +1,7 @@
 <?php
-$cwd = getcwd();
-include "../includes/connection.php";
-$job_title = mysqli_real_escape_string($connection, $_POST['job_title']);
-$url = mysqli_real_escape_string($connection, $_POST['job_url']);
+include "../includes/pg-connection.php";
+$job_title = pg_escape_string($connection, $_POST['job_title']);
+$url = pg_escape_string($connection, $_POST['job_url']);
 $remote = $_POST['remote'];
 $posting_date = $_POST['posting_date'];
 $deadline = $_POST['deadline'];
@@ -13,7 +12,7 @@ $company_id = $_POST['company_id'];
 if (isset($_POST['job_id']) && $_POST['job_id'] > 0) {
     $query = "UPDATE job...";
 } else {
-    $insertQuery = "INSERT INTO job(job_title, company_id, remote, url) VALUES('$job_title', $company_id, $remote, '$url') RETURNING job_id";
+    $insertQuery = "INSERT INTO job(job_title, company_id, remote, job_url) VALUES('$job_title', $company_id, '$remote', '$url') RETURNING job_id";
     if ($result = pg_query($connection, $insertQuery)) {
         $job_id = pg_fetch_result($result, 0, "job_id");
         echo json_encode(array("message" => "Successfully saved job information", "status" => 0, "job_id" => $job_id));
