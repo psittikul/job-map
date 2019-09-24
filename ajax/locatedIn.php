93718c87ae02e5b803849ad4b1ec4bccb8a36625
psittikul <patricia.sitt@gmail.com>
2019-09-24 04:38:42 -0400

    Working out job location functionality

--- ajax/locatedIn.php
+++ ajax/locatedIn.php
@@ -5,46 +5,42 @@ $object_id = $_POST['object_id'];
 $object_locations = $_POST['object_locations'];
 $status = 0;
 
-if (sizeof($object_type) > 1) {
+
+foreach ($object_locations as $location) {
+    $city = pg_escape_string($connection, trim(explode(", ", $location)[0]));
+    $state = pg_escape_string($connection, trim(explode(", ", $location)[1]));
+    $country = pg_escape_string($connection, trim(explode(", ", $location)[2]));
     /**
-     * If the object locations should be associated with both a job and a company, handle accordingly
-     */
-    $job_id = intval($object_id["job_id"]);
-    $company_id = intval($object_id["company_id"]);
-    echo json_encode(array("status"=>$status, "job_id"=>$job_id, "company_id"=>$company_id));
- } else {
-
-    foreach ($object_locations as $location) {
-        $city = pg_escape_string($connection, trim(explode(", ", $location)[0]));
-        $state = pg_escape_string($connection, trim(explode(", ", $location)[1]));
-        $country = pg_escape_string($connection, trim(explode(", ", $location)[2]));
-        /**
-         * Depending on the type of object(s) being associated with locations, insert into the company_located_in
-         * or job_located_in table accordingly
-         *  */
-        switch ($object_type) {
-            case "company":
-                $query = "INSERT INTO company_located_in(company_id, city, state, country) VALUES($object_id,
+     * Depending on the type of object(s) being associated with locations, insert into the company_located_in
+     * or job_located_in table accordingly
+     *  */
+    switch ($object_type) {
+        case "company":
+            $query = "INSERT INTO company_located_in(company_id, city, state, country) VALUES($object_id,
         '$city', '$state', '$country')";
-                break;
-            case "job":
-                $query = "INSERT INTO job_located_in(job_id, city, state, country) VALUES($object_id,
+            break;
+        case "job":
+            $query = "INSERT INTO job_located_in(job_id, city, state, country) VALUES($object_id,
         '$city', '$state', '$country')";
-                break;
-        }
-        if ($result = pg_query($connection, $query)) {
-            $status = 0;
-        } else {
-            $status = -1;
-        }
+            break;
+        case ["job", "company"]:
+            $job_id = intval($object_id["job_id"]);
+            $company_id = intval($object_id["company_id"]);
+            $query = "INSERT INTO job_located_in(job_id, city, state, country) VALUES($job_id, '$city','$state', '$country');";
+            break;
     }
-
-    if ($status == 0) {
-        echo json_encode(array(
-            "status" => $status, "message" => "Successfully inserted into located_in", "object_id" => $object_id,
-            "object_type" => $object_type
-        ));
+    if ($result = pg_query($connection, $query)) {
+        $status = 0;
     } else {
-        echo json_encode(array("status" => $status, "message" => "ERROR: " . pg_last_error($connection)));
+        $status = -1;
     }
+}
+
+if ($status == 0) {
+    echo json_encode(array(
+        "status" => $status, "message" => "Successfully inserted into located_in", "object_id" => $object_id,
+        "object_type" => $object_type
+    ));
+} else {
+    echo json_encode(array("status" => $status, "message" => "ERROR: " . pg_last_error($connection)));
 }
